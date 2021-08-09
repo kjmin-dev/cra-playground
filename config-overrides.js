bdb970e9f1d72f87319f668c1e654b6255bc0668
kjmin <kjmin@covision.co.kr>
2021-08-09 18:04:33 +0900

    Add swc-loader

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,32 @@
 const { alias, aliasJest, configPaths } = require('react-app-rewire-alias');
-
+const swcLoader = require('swc-loader');
 const aliasMap = configPaths('./tsconfig.paths.json');
+
+function addSwcLoader(config = {}) {
+    config.module.rules.push({
+        test: /\.(js|ts|tsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: require.resolve('swc-loader'),
+            options: {
+                parseMap: true,
+                inputSourceMap: true,
+                jsc: {
+                    parser: {
+                        syntax: 'typescript',
+                        tsx: true
+                    }
+                }
+            }
+        }
+    });
+    return config;
+}
 
 function overrideConfig({ usePreact = false, useJest = false } = {}) {
     return (config, env) => {
+        addSwcLoader(config);
+
         if (usePreact || env === 'production') {
             config.resolve.alias['react'] = 'preact/compat';
             config.resolve.alias['react-dom'] = 'preact/compat';
